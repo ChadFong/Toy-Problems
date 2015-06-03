fe541047fe87f4b2a495ce40952961f69f4e3080
ChadFong <ChadMFong@Gmail.com>
2015-06-03 14:19:09 -0700

    (add) C# solution to problem 2

--- ProjectEuler_CSharp/ProjectEuler_CSharp/Main.cs
+++ ProjectEuler_CSharp/ProjectEuler_CSharp/Main.cs
@@ -11,6 +11,7 @@ namespace ProjectEuler_CSharp
 		private static void Check_1_Thru_10 ()
 		{
 			Console.WriteLine ("001: Sum should equal 233168: {0}", Problems_1_Thru_10.Multiples3and5 () == 233168);
+			Console.WriteLine ("002: Sum should equal 4613732: {0}", Problems_1_Thru_10.EvenFib () == 4613732);
 		}
 	}
 }
