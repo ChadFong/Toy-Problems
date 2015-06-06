fdaef723e6277319f6dbc38273ad726948b0fd58
ChadFong <ChadMFong@Gmail.com>
2015-06-06 12:21:46 -0700

    (add) solve #8: Largest Product in Series

--- ProjectEuler_CSharp/ProjectEuler_CSharp/Main.cs
+++ ProjectEuler_CSharp/ProjectEuler_CSharp/Main.cs
@@ -8,7 +8,7 @@ namespace ProjectEuler_CSharp
 		public static void Main (string[] args)
 		{
 			Check_1_Thru_10 ();
-//            Console.WriteLine ("008: Largest product in Series? {0}", Problems_1_Thru_10.);
+//            Console.WriteLine ("009:  {0}", Problems_1_Thru_10.);
 		}
 		private static void Check_1_Thru_10 ()
 		{
@@ -19,6 +19,7 @@ namespace ProjectEuler_CSharp
             Console.WriteLine ("005: Smallest multiple of range 1 to 20 is: 232792560? {0}", Problems_1_Thru_10.smallestMultiple(20) == 232792560);
             Console.WriteLine ("006: Difference between Square of the sums to the sum of the squares from 1 to 100 is: 25164150? {0}", Problems_1_Thru_10.sumSquareDifference(100) == 25164150);
             Console.WriteLine ("007: 10,001st Prime is 104743? {0}", Problems_1_Thru_10.NthPrime(10001) == 104743);
+            Console.WriteLine ("008: Largest product in Series is 23514624000? {0}", Problems_1_Thru_10.largestProductInSeries(13) == 23514624000);
 		}
 	}
 }
